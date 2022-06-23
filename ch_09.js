918275f5a63d0580b6c002aeba3f29d7157fe9bd
khj <khj9872100@gmail.com>
2022-06-23 14:53:32 +0900

    git ignore test

--- ch_09.js
+++ ch_09.js
@@ -45,7 +45,76 @@ console.log(3); // 3번
     }
 }
 
-어디서 뭐 하고 넘겨주고, 그 결과값으로 넘겨서 또다른 콜백을 하고....
+어디서 뭐 하고 넘겨주고, 그 결과값으로 넘겨서 또다른 콜백을 하고.... 엄청 중첩돼서 복잡해짐
 이게 콜백헬인듯
 이거 해결해야돼 >> 프로미스 등장
+*/
+
+/* 프로미스: 객체
+js에서 제공하는 타입
+
+
+function func1(resolve, reject) {
+    let a = 1;
+    if (a===1) {
+        resolve("good");
+    } else {
+        reject("bad");
+    }
+}
+
+const result = new Promise(func1);
+console.log(result);
+
+ 저거 출력하면 good이 출력이 된다 프로미스는 객체다~ 근데 보통 저렇게 안씀
+*/
+
+/*
+보통은 아래처럼 쓴다
+
+resolve: 성공시
+reject: 실패시
+
+저 값을 받아서 then에 넘겨줌
+아마.. if 돌려보고 then을 해라
+if 결과값을 res로 넘겨주겠다
+
+저 then 다음에 또 then 이어서 붙일 수 있음
+>> a한 다음에 a_return 갖고 b하고, b한 다음에 b_return 갖고 c 하고....
+>> callback을 간단하게 쓸 수 있겠군
+
+catch: 에러 잡는것. 파라미터는 보통 e로 쓴다
+웹프 시간에 배웠던 try-catch랑 같은거 같음
+
+throw new Error: res가 good이면 에러를 띄워라
+에러가 잡히면 맨 아래 catch로 넘어가서 콘솔에 에러를 띄움
+*/
+
+new Promise((resolve, reject) => {
+    let a = 1;
+    if (a===1){
+        resolve("good");
+    } else {
+        reject("bad");
+    }
+})
+.then((res) => {
+    console.log(res);
+    if (res==="good"){
+        throw new Error("에러!");
+    }
+    return "hello"
+})
+.then((res) => {
+    console.log(res);
+})
+.catch((e) => {
+    console.error(e);
+});
+
+/* 통신 - HTTP
+ajax: 웹 동작중 비동기적으로 통신 > 사용자가 웹 동작중 다른 작업을 수행해도 비동기적으로 동작하기 때문에 알아서 필요한 작업을 수행, 웹 ui를 업데이트함
+(ex. 구글어스 - 지도를 클릭하면 프리징현상(멈춤) 없이 각 부분마다 api를 통해 통신하여 화면이 업데이트)
+
+api: 내가 지금 갖고있는 자원을 다른 사람들/장치에게 활용하도록 주는것
 */
---
+++ ch_10.js
@@ -0,0 +1,54 @@
+/* fetch
+json
+js에 가져오려면 문자형으로 가져와야함  보통 백틱으로 많이 쓰나봄
+활용하려면 객체로 만들어줘야함
+*/
+
+const data = `
+    {
+        "name": "hyunju",
+        "age" : 25
+    }
+`;
+
+const result=JSON.parse(data); // json을 객체로 만들어줌
+const result2=JSON.stringify(result); // 객체를 json으로 만들어줌
+/* 객체로 만든걸 왜 또 json으로 만드냐?
+json을 js에서 갖고 놀려면 객체로 만들어야 key, value를 뽑아올 수 있고 (parse)
+그걸 업데이트하거나 해서 다른 데로 보내주려면 다시 json으로 만들어야함 (stringfy)
+*/
+
+console.log(result2);
+
+/*fetch
+
+fetch('https://jsonplaceholder.typicode.com/todos/1')
+  .then(response => response.json())
+  .then(json => console.log(json))
+
+프로미스 형태임
+fetch(url)에서 데이터를 잘 가져오면 resolve 함수를 실행시켜서 then 2개 넘어갈 수 있고, 아니면 뭘 못하는데, catch를 추가로 붙여서 오류를 잡을수도 있음
+
+fetch는.. 잘 모르겠지만 확장 프로그램 느낌..? open with live server하면 잘 돌아가는데, nodejs에서 돌리면 오류남
+웹은 기본적으로 import 되어있는 느낌, nodejs에서는 기본제공은 아닌 느낌
+ㄴ 브라우저에서 제공하는 api라고 한다
+
+npm: 자바스크립트 라이브러리. 다른 사람들이 만든걸 쉽게 설치할 수 있도록함
+
+*/
+
+const fetch = require('node-fetch');
+//인증서 문제로, 아래 코드 추가하여 해결함
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+
+// new Promise ((resolve, reject) => {
+
+
+// })
+
+fetch('https://jsonplaceholder.typicode.com/todos/1')
+  .then(response => response.json())
+  .then(json => console.log(json))
+  .catch((e) => {
+    console.error(e)
+  });
